d301e0494cd14809a6e63e01dd17ae242bd11ad0
DOMINICKALUMBA <dominiquekalumba1000@gmail.com>
2024-11-18 14:29:30 +0300

    js

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+let isError = false;
 
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+console.log(entryDropdown.value)
